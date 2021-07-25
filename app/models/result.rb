888d21704ce0b62ccc12a0005f24f4d22cff97e0
Aiandrox <aiandrox7@gmail.com>
2021-07-25 23:29:06 +0900

    grouped_attendance_idsのロジックを移動

--- app/models/result.rb
+++ app/models/result.rb
@@ -23,30 +23,33 @@ class Result < ApplicationRecord
   belongs_to :event
 
   def self.group(event)
-    if event.criteria.present?
-      event.attendances.joins(attendance_statuses: :criterion_status).group_by(&:criterion_status_ids)
-    else
-      grouped_attendance_ids = event.attendance_ids.shuffle.in_groups(event.group_count, false)
-      transaction do
-        result = create!(
-          group_count: event.group_count,
-          uuid: SecureRandom.urlsafe_base64(9),
-          event_id: event.id
-        )
+    transaction do
+      result = create!(
+        group_count: event.group_count,
+        uuid: SecureRandom.urlsafe_base64(9),
+        event_id: event.id
+      )
 
-        grouped_attendance_ids.each do |attendance_ids|
-          group = result.groups.create!
-          attendance_ids.each do |attendance_id|
-            attendance = Attendance.find(attendance_id)
-            group.group_users.create!(user_name: attendance.user_name, user_id: attendance.user_id)
-          end
+      grouped_attendance_ids.each do |attendance_ids|
+        group = result.groups.create!
+        attendance_ids.each do |attendance_id|
+          attendance = Attendance.find(attendance_id)
+          group.group_users.create!(user_name: attendance.user_name, user_id: attendance.user_id)
         end
-        result
       end
+      result
     end
   end
 
   def to_param
     uuid
+  end
+
+  def grouped_attendance_ids
+    if event.criteria.present?
+      event.attendances.joins(attendance_statuses: :criterion_status).group_by(&:criterion_status_ids)
+    else
+      event.attendance_ids.shuffle.in_groups(event.group_count, false)
+    end
   end
 end
