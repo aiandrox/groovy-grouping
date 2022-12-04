05f365c72cdc81512e1f6e0f9a9829a056ed5ccf
Aiandrox <aiandrox7@gmail.com>
2022-12-04 14:53:10 +0900

    [Add] 通ってほしい落ちるテストを追加

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -53,43 +53,88 @@ RSpec.describe Result, type: :model do
     end
 
     context '条件があるとき' do
-      let!(:criterion) { create(:criterion, event: event) }
-      let!(:criterion_status_1) { create(:criterion_status, criterion: criterion) }
-      let!(:criterion_status_2) { create(:criterion_status, criterion: criterion) }
-      let!(:attendance_status_1) { create(:attendance_status, attendance: attendance_1, criterion_status: criterion_status_1) }
-      let!(:attendance_status_2) { create(:attendance_status, attendance: attendance_2, criterion_status: criterion_status_1) }
-      let!(:attendance_status_3) { create(:attendance_status, attendance: attendance_3, criterion_status: criterion_status_2) }
-      let!(:attendance_status_4) { create(:attendance_status, attendance: attendance_4, criterion_status: criterion_status_2) }
-      let!(:attendance_status_5) { create(:attendance_status, attendance: attendance_5, criterion_status: criterion_status_2) }
+      context '5人・条件2つ（2:3）' do
+        let!(:criterion) { create(:criterion, event: event) }
+        let!(:criterion_status_1) { create(:criterion_status, criterion: criterion) }
+        let!(:criterion_status_2) { create(:criterion_status, criterion: criterion) }
+        let!(:attendance_status_1) { create(:attendance_status, attendance: attendance_1, criterion_status: criterion_status_1) }
+        let!(:attendance_status_2) { create(:attendance_status, attendance: attendance_2, criterion_status: criterion_status_1) }
+        let!(:attendance_status_3) { create(:attendance_status, attendance: attendance_3, criterion_status: criterion_status_2) }
+        let!(:attendance_status_4) { create(:attendance_status, attendance: attendance_4, criterion_status: criterion_status_2) }
+        let!(:attendance_status_5) { create(:attendance_status, attendance: attendance_5, criterion_status: criterion_status_2) }
 
-      it '結果レコードを作成する' do
-        expect { subject }.to change(Result, :count).by(1)
-                          .and change(Group, :count).by(2)
-                          .and change(GroupUser, :count).by(5)
-                          .and change(LogCriterion, :count).by(1)
-                          .and change(LogUserStatus, :count).by(5)
+        it '結果レコードを作成する' do
+          expect { subject }.to change(Result, :count).by(1)
+                            .and change(Group, :count).by(2)
+                            .and change(GroupUser, :count).by(5)
+                            .and change(LogCriterion, :count).by(1)
+                            .and change(LogUserStatus, :count).by(5)
+        end
+
+        it '均等に2チームに分ける（2人グループ・3人グループ）' do
+          subject
+          result = Result.last
+          expect(result.groups.size).to eq 2
+          expect(result.group_count).to eq 2
+          group_1 = result.groups.first
+          group_2 = result.groups.last
+          expect(group_1.users.size).to eq 3
+          expect(group_2.users.size).to eq 2
+        end
+
+        it '条件で均等に分ける' do
+          subject
+          result = Result.last
+          group_1 = result.groups.first
+          group_2 = result.groups.last
+          group_1_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_1.users.ids })
+          group_2_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_2.users.ids })
+          expect(group_1_attendance_statuses.pluck(:criterion_status_id)).to include(criterion_status_1.id, criterion_status_2.id)
+          expect(group_2_attendance_statuses.pluck(:criterion_status_id)).to include(criterion_status_1.id, criterion_status_2.id)
+        end
       end
 
-      it '均等に2チームに分ける（2人グループ・3人グループ）' do
-        subject
-        result = Result.last
-        expect(result.groups.size).to eq 2
-        expect(result.group_count).to eq 2
-        group_1 = result.groups.first
-        group_2 = result.groups.last
-        expect(group_1.users.size).to eq 3
-        expect(group_2.users.size).to eq 2
-      end
+      context '7人・条件2つ（2:2:3）' do
+        let!(:criterion) { create(:criterion, event: event) }
+        let!(:criterion_status_1) { create(:criterion_status, criterion: criterion) }
+        let!(:criterion_status_2) { create(:criterion_status, criterion: criterion) }
+        let!(:attendance_status_1) { create(:attendance_status, attendance: attendance_1, criterion_status: criterion_status_1) }
+        let!(:attendance_status_2) { create(:attendance_status, attendance: attendance_2, criterion_status: criterion_status_1) }
+        let!(:attendance_status_3) { create(:attendance_status, attendance: attendance_3, criterion_status: criterion_status_2) }
+        let!(:attendance_status_4) { create(:attendance_status, attendance: attendance_4, criterion_status: criterion_status_2) }
+        let!(:attendance_status_5) { create(:attendance_status, attendance: attendance_5, criterion_status: criterion_status_2) }
 
-      it '条件で均等に分ける' do
-        subject
-        result = Result.last
-        group_1 = result.groups.first
-        group_2 = result.groups.last
-        group_1_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_1.users.ids })
-        group_2_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_2.users.ids })
-        expect(group_1_attendance_statuses.pluck(:criterion_status_id).uniq).to include(criterion_status_1.id, criterion_status_2.id)
-        expect(group_2_attendance_statuses.pluck(:criterion_status_id).uniq).to include(criterion_status_1.id, criterion_status_2.id)
+        let!(:attendance_6) { create(:attendance, event: event) }
+        let!(:attendance_7) { create(:attendance, event: event) }
+        let!(:attendance_status_6) { create(:attendance_status, attendance: attendance_6, criterion_status: criterion_status_1) }
+        let!(:attendance_status_7) { create(:attendance_status, attendance: attendance_7, criterion_status: criterion_status_2) }
+
+        it '均等に3チームに分ける（2人グループ・3人グループ）' do
+          subject
+          result = Result.last
+          expect(result.groups.size).to eq 3
+          expect(result.group_count).to eq 3
+          group_1 = result.groups.first
+          group_2 = result.groups.second
+          group_3 = result.groups.last
+          expect(group_1.users.size).to eq 3
+          expect(group_2.users.size).to eq 2
+          expect(group_2.users.size).to eq 2
+        end
+
+        it '条件で均等に分ける' do
+          subject
+          result = Result.last
+          group_1 = result.groups.first
+          group_2 = result.groups.second
+          group_3 = result.groups.last
+          group_1_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_1.users.ids })
+          group_2_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_2.users.ids })
+          group_3_attendance_statuses = AttendanceStatus.joins(:attendance).where(attendances: { user_id: group_3.users.ids })
+          expect(group_1_attendance_statuses.pluck(:criterion_status_id)).to include(criterion_status_1.id, criterion_status_2.id)
+          expect(group_2_attendance_statuses.pluck(:criterion_status_id)).to include(criterion_status_1.id, criterion_status_2.id)
+          expect(group_3_attendance_statuses.pluck(:criterion_status_id)).to include(criterion_status_1.id, criterion_status_2.id)
+        end
       end
     end
   end
